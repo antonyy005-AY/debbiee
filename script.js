3d172792e1bddafdbffc37c5727ca1bb02b65aee
antonyy005-AY <antonyy005@gmail.com>
2026-08-01 21:47:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,22 +4,33 @@ const closePopup = document.getElementById("closePopup");
 const newMsgBtn = document.getElementById("newMsgBtn");
 const mainMessage = document.getElementById("mainMessage");
 const card = document.getElementById("birthdayCard");
+const balloonBtn = document.getElementById("balloonBtn");
+const balloonModal = document.getElementById("balloonModal");
+const closeBalloon = document.getElementById("closeBalloon");
+const balloonMessage = document.getElementById("balloonMessage");
+const balloons = document.querySelectorAll(".balloon");
+const lastPopup = document.getElementById("lastPopup");
 
+popup.style.display = "none";
+lastPopup.style.display = "none";
 
 const messages = [
-  "You are my favorite person, and today is all about you 💕",
-  "May your smile stay bright and your dreams come true ✨",
-  "You deserve cake, love, laughter, and endless happiness 🎂"
+  "Having you as my best friend is the greatest gift; may your birthday be full of love. 💕 - Josh",
+  "Thank you for holding the umbrella over my head during every emotional storm I had to navigate. ✨ - YOvvv",
+  "You’re the sibling I got to choose for myself, and I would choose you in every single lifetime. 🎂 - Vasu"
 ];
 
 let msgIndex = 0;
 
 surpriseBtn.addEventListener("click", () => {
+  balloonModal.style.display = "none";
   popup.style.display = "flex";
 });
 
 closePopup.addEventListener("click", () => {
-  popup.style.display = "none";
+	window.location.reload();
+  //popup.style.display = "none";
+  //lastPopup.style.display="flex";
 });
 
 newMsgBtn.addEventListener("click", () => {
@@ -38,28 +49,40 @@ card.addEventListener("mouseleave", () => {
 });
 
 
-const balloonBtn = document.getElementById("balloonBtn");
-const balloonModal = document.getElementById("balloonModal");
-const closeBalloon = document.getElementById("closeBalloon");
-const balloonMessage = document.getElementById("balloonMessage");
-const balloons = document.querySelectorAll(".balloon");
+
+
 
 balloonBtn.addEventListener("click", () => {
   balloonModal.style.display = "flex";
 });
 
-closeBalloon.addEventListener("click", () => {
+/*closeBalloon.addEventListener("click", () => {
   balloonModal.style.display = "none";
   balloons.forEach(balloon => {
     balloon.classList.remove("popped");
   });
   balloonMessage.textContent = "Click a balloon to see a message.";
 });
-
+*/
 balloons.forEach(balloon => {
   balloon.addEventListener("click", () => {
     const msg = balloon.getAttribute("data-msg");
+	const id = balloon.getAttribute("id");
     balloon.classList.add("popped");
-    balloonMessage.textContent = msg;
+	document.getElementById(id+"msg").value =msg;
+	const balloonMessage = document.getElementById(id+"msg");
+	balloonMessage.textContent = msg;
   });
 });
+
+
+const envelopeWrap = document.getElementById("envelopeWrap");
+
+ envelopeWrap.addEventListener("click", function () {
+   envelopeWrap.classList.add("open");
+ });
+
+ closePopup.addEventListener("click", function (e) {
+   e.stopPropagation();
+   document.getElementById("popup").style.display = "none";
+ });
